c04c196dfc59b091b8b9a73a1e0ea0dd3692c003
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:56:20 -0500

    Trying alternate static colors now that the right pixel byte density and order are figured out

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ num_pixels = 300
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, bpp=3, auto_write=False, pixel_order=neopixel.GRB)
 
-ORANGE = (239, 108, 0)
-PURPLE = (142, 36, 170)
+ORANGE = (108, 239, 0) # In GRB
+PURPLE = (36, 142, 170) # In GRB
 
 for i in range(0,num_pixels):
     pixels[i] = ORANGE if i % 2 == 0 else PURPLE
