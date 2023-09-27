488e8ba75a3c8da1693bd06b33fbe772f39df820
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:10:35 -0500

    Update

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ BRIGHTNESS = 0.25
 FRONT_PIN = board.D18 # physical 12
 REAR_PIN = board.D12
 
-ORANGE = (239, 108, 0)
-PURPLE = (142, 36, 170)
+ORANGE = (239, 108, 0, 0)
+PURPLE = (142, 36, 170, 0)
 
 front = neopixel.NeoPixel(FRONT_PIN, COUNT, brightness=BRIGHTNESS, auto_write=False, pixel_order=(1,0,2,3))
-rear = neopixel.NeoPixel(REAR_PIN, COUNT)
+front.fill(ORANGE)
 
-def init():
-    for x in range(0, COUNT):
-        front[x] = (239, 108, 0) if x % 2 == 0 else (142, 36, 170)
-    front.show()
-    # main()
+# def init():
+#     for x in range(0, COUNT):
+#         front[x] = (239, 108, 0) if x % 2 == 0 else (142, 36, 170)
+#     front.show()
+#     # main()
 
-def main():
-    while (1==1):
-        time.sleep(3)
-        for x in range(0, COUNT):
-            front[x] = ORANGE if front[x] == PURPLE else PURPLE
+# def main():
+#     while (1==1):
+#         time.sleep(3)
+#         for x in range(0, COUNT):
+#             front[x] = ORANGE if front[x] == PURPLE else PURPLE
 
 
-init()
+# init()
