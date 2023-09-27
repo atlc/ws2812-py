afee8d25451ca8cfd857494d2c3d091492b704d5
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:50:01 -0500

    Update

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ num_pixels = 300
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, bpp=4, auto_write=False, pixel_order=neopixel.GRBW)
 
-pixels.fill((239,108,0))
+pixels.fill((239,108,0,0))
 pixels.show()
