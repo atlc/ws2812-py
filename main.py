981bc0f3f3d6a1a23a236652e7d3789a283cc847
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:05:06 -0500

    Adjusting formatting of the light strip

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import neopixel
 import time
 
 COUNT = 300 # 5 meter strips at 60 leds/meter
-FRONT_PIN = board.D5 # GPIO pin 5, physical pin 29
-REAR_PIN = board.D6 # GPIO pin 6, physical pin 31
+BRIGHTNESS = 0.25
+FRONT_PIN = board.D18 # physical 12
+REAR_PIN = board.D12
 
 ORANGE = (239, 108, 0)
 PURPLE = (142, 36, 170)
 
-front = neopixel.NeoPixel(FRONT_PIN, COUNT)
+front = neopixel.NeoPixel(FRONT_PIN, COUNT, brightness=BRIGHTNESS, auto_write=False, pixel_order=(1,0,2,3))
 rear = neopixel.NeoPixel(REAR_PIN, COUNT)
+
+front.fill(0,0,0)
 
 def init():
     for x in range(0, COUNT):
-        front[x] = ORANGE if x % 2 == 0 else PURPLE
-    main()
+        front[x] = (239, 108, 0) if x % 2 == 0 else (142, 36, 170)
+    front.show()
+    # main()
 
 def main():
     while (1==1):
@@ -23,5 +27,5 @@ def main():
         for x in range(0, COUNT):
             front[x] = ORANGE if front[x] == PURPLE else PURPLE
 
-        
+
 init()
