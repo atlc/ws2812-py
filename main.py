6947535cbb871eef29a85ede6dd3fc49580376eb
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:59:03 -0500

    Update

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ num_pixels = 300
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, bpp=3, auto_write=False, pixel_order=neopixel.GRB)
 
-ORANGE = (108, 239, 0) # In GRB
-PURPLE = (142, 170) # In GRB
+ORANGE = (239, 108, 0)
+# PURPLE = (142, 170)
 
 for i in range(0,num_pixels):
-    pixels[i] = ORANGE if i % 2 == 0 else (0,0,0)
+    pixels[i] = ORANGE if i % 2 == 0 else (10,10,10)
 
 pixels.show()
