c96cab8d3d1c9b49e03621ba7ec1c89afa6555ac
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 18:47:32 -0500

    Update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 300
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, bpp=4, auto_write=False, pixel_order=neopixel.RGBW)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, bpp=4, auto_write=False, pixel_order=neopixel.RGB)
 
 pixels.fill((239,108,0,0))
 pixels.show()
