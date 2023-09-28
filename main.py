3956da6ea3b0799329b0bc190efafba1311dc256
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 19:26:19 -0500

    Update

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 300
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, bpp=3, auto_write=False, pixel_order=neopixel.GRB)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5,
+                           bpp=3, auto_write=False, pixel_order=neopixel.GRB)
 
 ORANGE = (239, 80, 0)
 PURPLE = (95, 0, 95)
 GREEN = (15, 100, 15)
 
-color_cycle = 1
 
-def get_color_cycle():
-    if (color_cycle % 3 == 0): return ORANGE
-    if (color_cycle % 3 == 1): return PURPLE
-    if (color_cycle % 3 == 2): return GREEN
+def static_tricolor():
+    color_cycle = 1
 
-for i in range(0, num_pixels):
-    if (i % 4 == 0): color_cycle += 1
-    pixels[i] = get_color_cycle()
+    def get_color_cycle():
+        if (color_cycle % 3 == 0):
+            return ORANGE
+        if (color_cycle % 3 == 1):
+            return PURPLE
+        if (color_cycle % 3 == 2):
+            return GREEN
 
-pixels.show()
+    for i in range(0, num_pixels):
+        if (i % 4 == 0):
+            color_cycle += 1
+        pixels[i] = get_color_cycle()
+
+    pixels.show()
+
+
+def moving_dual_color():
+    for i in range(0, num_pixels):
+        pixels[i] = ORANGE if i < num_pixels / 2 else PURPLE
+
+moving_dual_color()
