c37f2b8252e54d0d013c582cf8084abe269be838
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 20:04:44 -0500

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 pixel_pin = board.D18
 num_pixels = 600
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1,
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.8,
                            bpp=3, auto_write=False, pixel_order=neopixel.GRB)
 
 ORANGE = (239, 80, 0)
