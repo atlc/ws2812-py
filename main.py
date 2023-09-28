45f4f804dc7105acd1cb0923e1a3543dd0b6d4a2
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-27 19:01:26 -0500

    Update

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 300
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, bpp=3, auto_write=False, pixel_order=neopixel.GRB)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, bpp=3, auto_write=True, pixel_order=neopixel.GRB)
 
 ORANGE = (239, 80, 0)
 PURPLE = (142, 0, 170)
 
 for i in range(0,num_pixels):
     pixels[i] = ORANGE if i % 2 == 0 else PURPLE
-
-pixels.show()
